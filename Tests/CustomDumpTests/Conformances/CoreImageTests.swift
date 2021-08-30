b1d34e0711419acb251ad71bf34c5e27424f5440
stephencelis <stephencelis@users.noreply.github.com>
2021-08-30 17:32:42 +0000

    Run swift-format

--- Tests/CustomDumpTests/Conformances/CoreImageTests.swift
+++ Tests/CustomDumpTests/Conformances/CoreImageTests.swift
@@ -1,40 +1,39 @@
 #if canImport(CoreImage)
-import CoreImage
-import CustomDump
-import XCTest
+  import CoreImage
+  import CustomDump
+  import XCTest
 
-
-final class CoreImageTests: XCTestCase {
+  final class CoreImageTests: XCTestCase {
     func testCIQRCodeDescriptor() {
-        var dump = ""
-        customDump(
-            [.levelH, .levelL, .levelM, .levelQ] as [CIQRCodeDescriptor.ErrorCorrectionLevel],
-            to: &dump
-        )
-        
-        XCTAssertEqual(
-            dump,
-            """
-      [
-        [0]: CIQRCodeDescriptor.ErrorCorrectionLevel.levelH,
-        [1]: CIQRCodeDescriptor.ErrorCorrectionLevel.levelL,
-        [2]: CIQRCodeDescriptor.ErrorCorrectionLevel.levelM,
-        [3]: CIQRCodeDescriptor.ErrorCorrectionLevel.levelQ
-      ]
-      """
-        )
-        
-        dump = ""
-        customDump(
-            CIQRCodeDescriptor.ErrorCorrectionLevel.levelH,
-            to: &dump
-        )
-        XCTAssertEqual(
-            dump,
-            """
-      CIQRCodeDescriptor.ErrorCorrectionLevel.levelH
-      """
-        )
+      var dump = ""
+      customDump(
+        [.levelH, .levelL, .levelM, .levelQ] as [CIQRCodeDescriptor.ErrorCorrectionLevel],
+        to: &dump
+      )
+
+      XCTAssertEqual(
+        dump,
+        """
+        [
+          [0]: CIQRCodeDescriptor.ErrorCorrectionLevel.levelH,
+          [1]: CIQRCodeDescriptor.ErrorCorrectionLevel.levelL,
+          [2]: CIQRCodeDescriptor.ErrorCorrectionLevel.levelM,
+          [3]: CIQRCodeDescriptor.ErrorCorrectionLevel.levelQ
+        ]
+        """
+      )
+
+      dump = ""
+      customDump(
+        CIQRCodeDescriptor.ErrorCorrectionLevel.levelH,
+        to: &dump
+      )
+      XCTAssertEqual(
+        dump,
+        """
+        CIQRCodeDescriptor.ErrorCorrectionLevel.levelH
+        """
+      )
     }
-}
+  }
 #endif
