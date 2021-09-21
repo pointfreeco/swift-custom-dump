897071de6bee4ff162c62a07f7ea28f184f810b6
Kevin Lundberg <kevin@klundberg.com>
2021-09-21 09:41:37 -0400

    Don't compile symbols for mac catalyst that are not yet available out of beta (#20)

--- Sources/CustomDump/Conformances/UserNotifications.swift
+++ Sources/CustomDump/Conformances/UserNotifications.swift
@@ -106,7 +106,7 @@
   }
 
   // NB: Xcode 13 does not include macOS 12 SDK
-  #if compiler(>=5.5) && !os(macOS)
+  #if compiler(>=5.5) && !os(macOS) && !targetEnvironment(macCatalyst)
     @available(iOS 15, macOS 12, tvOS 15, watchOS 8, *)
     extension UNNotificationInterruptionLevel: CustomDumpStringConvertible {
       public var customDumpDescription: String {
