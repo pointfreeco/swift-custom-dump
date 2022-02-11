ff3bc38566969906aeea23936a85ea226aca4d77
Stephen Celis <stephen@stephencelis.com>
2022-02-11 17:17:41 -0500

    wip

--- Tests/CustomDumpTests/Conformances/SwiftUITests.swift
+++ Tests/CustomDumpTests/Conformances/SwiftUITests.swift
@@ -1,8 +1,10 @@
 #if canImport(SwiftUI) && DEBUG
   import CustomDump
   import Foundation
+  import SwiftUI
   import XCTest
 
+  @available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
   final class SwiftUITests: XCTestCase {
     func testSwiftUI() {
       var dump = ""
