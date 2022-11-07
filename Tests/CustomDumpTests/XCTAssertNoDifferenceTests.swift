2c845077b8076a277537e319863873f9d6e7358e
Stephen Celis <stephen@stephencelis.com>
2022-11-07 09:58:55 -0800

    wip

--- Tests/CustomDumpTests/XCTAssertNoDifferenceTests.swift
+++ Tests/CustomDumpTests/XCTAssertNoDifferenceTests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 @available(iOS 13, macOS 10.15, tvOS 13, watchOS 6, *)
 class XCTAssertNoDifferenceTests: XCTestCase {
-  #if compiler(>=5.4) && (os(iOS) || os(macOS) || os(tvOS) || os(watchOS))
+  #if DEBUG && compiler(>=5.4) && (os(iOS) || os(macOS) || os(tvOS) || os(watchOS))
     func testXCTAssertNoDifference() {
       XCTExpectFailure()
 
