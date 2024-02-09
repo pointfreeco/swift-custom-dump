941e19835bec0bf1ac1cc49409842ec077c1b211
Stephen Celis <stephen@stephencelis.com>
2024-02-09 12:52:24 -0800

    wip

--- Sources/CustomDump/Diff.swift
+++ Sources/CustomDump/Diff.swift
@@ -326,9 +326,8 @@ public func diff<T>(_ lhs: T, _ rhs: T, format: DiffFormat = .default) -> String
           terminator: "",
           to: &out
         )
-      } else if lhsItem == rhsItem,
-        let (lhs, rhs) = (lhs as? any _CustomDiffObject)?._customDiffValues
-      {
+      } else if lhsItem == rhsItem {
+        let (lhs, rhs) = lhs._customDiffValues
         print(
           diffHelp(
             lhs,
