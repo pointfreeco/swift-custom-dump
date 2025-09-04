e23672ec063e656634dc6f708737623c2c278157
Brandon Williams <mbrandonw@hey.com>
2025-09-04 10:01:02 -0500

    Update readme with screenshots.

---
+++ Tests/CustomDumpTests/UserTests.swift
@@ -0,0 +1,30 @@
+
+
+    @Test func students() {
+      #expect(student1 == student2)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let student1 = Student(favoriteNumbers: [42, 1729], id: 1, name: "Blob")
+let student2 = Student(favoriteNumbers: [42, 1729], id: 1, name: "Blob Jr")
+import CustomDump
+import Testing
+
+struct Student: Equatable {
+  var favoriteNumbers: [Int]
+  var id: Int
+  var name: String
+}
