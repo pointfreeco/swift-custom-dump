68daec1da0cd9ccd2034d6612b46d8f5e915dde9
Stephen Celis <stephen.celis@gmail.com>
2025-11-14 12:10:42 -0800

    Add dependency on IssueReportingTestSupport in tests

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -33,7 +33,8 @@ let package = Package(
     .testTarget(
       name: "CustomDumpTests",
       dependencies: [
-        "CustomDump"
+        "CustomDump",
+        .product(name: "IssueReportingTestSupport", package: "xctest-dynamic-overlay"),
       ]
     ),
   ]
