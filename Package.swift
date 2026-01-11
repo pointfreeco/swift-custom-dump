b592d200bc012c7b5d7c0faa24c1a4b05087e55c
Johan Kool <johankool@users.noreply.github.com>
2026-01-11 16:34:31 +0100

    Another attempt (8)

--- Package.swift
+++ Package.swift
@@ -1,7 +1,8 @@
-// swift-tools-version: 6.2.3
+// swift-tools-version: 6.0
 
 import PackageDescription
 
+#if !os(wasi)
 let package = Package(
   name: "swift-custom-dump",
   platforms: [
@@ -31,26 +32,43 @@ let package = Package(
       name: "CustomDumpTests",
       dependencies: [
         "CustomDump",
-        .product(
-          name: "IssueReportingTestSupport",
-          package: "xctest-dynamic-overlay",
-          condition: .when(
-            platforms: [
-              .android,
-              .driverKit,
-              .iOS,
-              .linux,
-              .macCatalyst,
-              .macOS,
-              .openbsd,
-              .tvOS,
-              .visionOS,
-              .watchOS,
-              .windows,
-            ] // Everything but wasi
-          )
-        ),
+        .product(name: "IssueReportingTestSupport", package: "xctest-dynamic-overlay"),
       ]
     ),
   ]
 )
+#else
+let package = Package(
+  name: "swift-custom-dump",
+  platforms: [
+    .iOS(.v13),
+    .macOS(.v10_15),
+    .tvOS(.v13),
+    .watchOS(.v6),
+  ],
+  products: [
+    .library(
+      name: "CustomDump",
+      targets: ["CustomDump"]
+    )
+  ],
+  dependencies: [
+    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.2.2")
+  ],
+  targets: [
+    .target(
+      name: "CustomDump",
+      dependencies: [
+        .product(name: "IssueReporting", package: "xctest-dynamic-overlay"),
+        .product(name: "XCTestDynamicOverlay", package: "xctest-dynamic-overlay"),
+      ]
+    ),
+    .testTarget(
+      name: "CustomDumpTests",
+      dependencies: [
+        "CustomDump",
+      ]
+    ),
+  ]
+)
+#endif
--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -2,6 +2,7 @@
 
 import PackageDescription
 
+#if !os(wasi)
 let package = Package(
   name: "swift-custom-dump",
   platforms: [
@@ -34,26 +35,46 @@ let package = Package(
       name: "CustomDumpTests",
       dependencies: [
         "CustomDump",
-        .product(
-          name: "IssueReportingTestSupport",
-          package: "xctest-dynamic-overlay",
-          condition: .when(
-            platforms: [
-              .android,
-              .driverKit,
-              .iOS,
-              .linux,
-              .macCatalyst,
-              .macOS,
-              .openbsd,
-              .tvOS,
-              .visionOS,
-              .watchOS,
-              .windows,
-            ] // Everything but wasi
-          )
-        ),
+        .product(name: "IssueReportingTestSupport", package: "xctest-dynamic-overlay"),
       ]
     ),
   ]
 )
+#else
+let package = Package(
+  name: "swift-custom-dump",
+  platforms: [
+    .iOS(.v13),
+    .macOS(.v10_15),
+    .tvOS(.v13),
+    .watchOS(.v6),
+  ],
+  products: [
+    .library(
+      name: "CustomDump",
+      targets: ["CustomDump"]
+    )
+  ],
+  dependencies: [
+    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.2.2")
+  ],
+  targets: [
+    .target(
+      name: "CustomDump",
+      dependencies: [
+        .product(name: "IssueReporting", package: "xctest-dynamic-overlay"),
+        .product(name: "XCTestDynamicOverlay", package: "xctest-dynamic-overlay"),
+      ],
+      swiftSettings: [
+        .enableExperimentalFeature("StrictConcurrency")
+      ]
+    ),
+    .testTarget(
+      name: "CustomDumpTests",
+      dependencies: [
+        "CustomDump",
+      ]
+    ),
+  ]
+)
+#endif
