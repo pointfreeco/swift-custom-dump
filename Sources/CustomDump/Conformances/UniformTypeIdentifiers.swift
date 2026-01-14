ef14a56817de4011f708d4c1fd2188b80343ac4a
stephencelis <stephencelis@users.noreply.github.com>
2026-01-14 20:31:32 +0000

    Run swift-format

--- Sources/CustomDump/Conformances/UniformTypeIdentifiers.swift
+++ Sources/CustomDump/Conformances/UniformTypeIdentifiers.swift
@@ -1,10 +1,10 @@
 #if canImport(UniformTypeIdentifiers)
-import UniformTypeIdentifiers
+  import UniformTypeIdentifiers
 
-@available(macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0, *)
-extension UTType: CustomDumpStringConvertible {
-  public var customDumpDescription: String {
-    "UTType(\(identifier))"
+  @available(macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0, *)
+  extension UTType: CustomDumpStringConvertible {
+    public var customDumpDescription: String {
+      "UTType(\(identifier))"
+    }
   }
-}
 #endif
