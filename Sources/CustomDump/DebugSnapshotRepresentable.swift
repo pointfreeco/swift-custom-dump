7e93bd7b0bd44ee431dc559f0a193fdeb171f588
Stephen Celis <stephen@stephencelis.com>
2026-02-07 12:44:54 -0800

    wip

--- Sources/CustomDump/DebugSnapshotRepresentable.swift
+++ Sources/CustomDump/DebugSnapshotRepresentable.swift
@@ -2,3 +2,15 @@ public protocol DebugSnapshotRepresentable<DebugSnapshot> {
   associatedtype DebugSnapshot
   var _debugSnapshot: DebugSnapshot { get }
 }
+
+extension Optional: DebugSnapshotRepresentable where Wrapped: DebugSnapshotRepresentable {
+  public var _debugSnapshot: Wrapped.DebugSnapshot? {
+    self?._debugSnapshot
+  }
+}
+
+extension Array: DebugSnapshotRepresentable where Element: DebugSnapshotRepresentable {
+  public var _debugSnapshot: [Element.DebugSnapshot] {
+    map(\._debugSnapshot)
+  }
+}
