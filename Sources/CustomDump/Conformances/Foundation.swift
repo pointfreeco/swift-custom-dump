0fc8018b2903e6ce471eb458ab23f8fc0ba6fdf6
Måns Bernhardt <mans.bernhardt@gmail.com>
2026-05-18 18:44:09 +0200

    Make sure to guard as well with canImport for FoundationNetworking

--- Sources/CustomDump/Conformances/Foundation.swift
+++ Sources/CustomDump/Conformances/Foundation.swift
@@ -232,7 +232,7 @@ extension NSURLQueryItem: CustomDumpRepresentable {
   }
 }
 
-#if FoundationNetworking
+#if FoundationNetworking && canImport(FoundationNetworking)
   extension NSURLRequest: CustomDumpRepresentable {
     public var customDumpValue: Any {
       self as URLRequest
@@ -273,7 +273,7 @@ extension URL: CustomDumpStringConvertible {
   }
 }
 
-#if FoundationNetworking
+#if FoundationNetworking && canImport(FoundationNetworking)
   extension URLRequest.NetworkServiceType: CustomDumpStringConvertible {
     public var customDumpDescription: String {
       switch self { #if canImport(FoundationNetworking)
