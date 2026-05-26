b9b59eb58c946236d6f16305c576ad194c36444e
stephencelis <stephencelis@users.noreply.github.com>
2026-05-26 19:48:24 +0000

    Run swift-format

--- Tests/CustomDumpTests/ExpectDifferenceTests.swift
+++ Tests/CustomDumpTests/ExpectDifferenceTests.swift
@@ -1,58 +1,58 @@
 import CustomDump
-import Testing
 import IssueReportingTestSupport
+import Testing
 
 #if !os(Android)
-struct ExpectDifferenceTests {
-  @Test func basics() {
-    var user = User(id: 42, name: "Blob")
-    func increment<Value>(_ root: inout Value, at keyPath: WritableKeyPath<Value, Int>) {
-      root[keyPath: keyPath] += 1
-    }
+  struct ExpectDifferenceTests {
+    @Test func basics() {
+      var user = User(id: 42, name: "Blob")
+      func increment<Value>(_ root: inout Value, at keyPath: WritableKeyPath<Value, Int>) {
+        root[keyPath: keyPath] += 1
+      }
 
-    expectDifference(user) {
-      increment(&user, at: \.id)
-    } changes: {
-      $0.id = 43
-    }
-  }
-
-  @Test func nonExhaustive() {
-    var user = User(id: 42, name: "Blob")
-    user.id += 1
-    user.name += " Jr"
-    expectDifference(user) {
-      $0.name = "Blob Jr"
-    }
-  }
-
-  @Test func failure() {
-    var user = User(id: 42, name: "Blob")
-    func increment<Value>(_ root: inout Value, at keyPath: WritableKeyPath<Value, Int>) {
-      root[keyPath: keyPath] += 1
-    }
-
-    withKnownIssue {
       expectDifference(user) {
         increment(&user, at: \.id)
       } changes: {
-        $0.id = 44
+        $0.id = 43
       }
-    } matching: {
-      $0.description.hasSuffix(
-        """
-        Difference: …
+    }
 
-            User(
-          −   id: 44,
-          +   id: 43,
-              name: "Blob"
-            )
+    @Test func nonExhaustive() {
+      var user = User(id: 42, name: "Blob")
+      user.id += 1
+      user.name += " Jr"
+      expectDifference(user) {
+        $0.name = "Blob Jr"
+      }
+    }
 
-        (Expected: −, Actual: +)
-        """
-      )
+    @Test func failure() {
+      var user = User(id: 42, name: "Blob")
+      func increment<Value>(_ root: inout Value, at keyPath: WritableKeyPath<Value, Int>) {
+        root[keyPath: keyPath] += 1
+      }
+
+      withKnownIssue {
+        expectDifference(user) {
+          increment(&user, at: \.id)
+        } changes: {
+          $0.id = 44
+        }
+      } matching: {
+        $0.description.hasSuffix(
+          """
+          Difference: …
+
+              User(
+            −   id: 44,
+            +   id: 43,
+                name: "Blob"
+              )
+
+          (Expected: −, Actual: +)
+          """
+        )
+      }
     }
   }
-}
 #endif
--- Tests/CustomDumpTests/ExpectNoDifferenceTests.swift
+++ Tests/CustomDumpTests/ExpectNoDifferenceTests.swift
@@ -1,42 +1,42 @@
 import CustomDump
 import Foundation
-import Testing
 import IssueReportingTestSupport
+import Testing
 
 #if !os(Android)
-struct ExpectNoDifferenceTests {
-  @Test func basics() {
-    struct User: Equatable {
-      var id: UUID
-      var name: String
-      var bio: String
-    }
-    let user = User(
-      id: UUID(uuidString: "DEADBEEF-DEAD-BEEF-DEAD-BEEFDEADBEEF")!,
-      name: "Blob",
-      bio: "Blobbed around the world."
-    )
-    var otherUser = user
-    expectNoDifference(user, otherUser)
-    otherUser.name += " Jr."
-    withKnownIssue {
-      expectNoDifference(user, otherUser)
-    } matching: {
-      $0.description.hasSuffix(
-        """
-        Difference: …
-
-            ExpectNoDifferenceTests.User(
-              id: UUID(DEADBEEF-DEAD-BEEF-DEAD-BEEFDEADBEEF),
-          −   name: "Blob",
-          +   name: "Blob Jr.",
-              bio: "Blobbed around the world."
-            )
-
-        (First: −, Second: +)
-        """
+  struct ExpectNoDifferenceTests {
+    @Test func basics() {
+      struct User: Equatable {
+        var id: UUID
+        var name: String
+        var bio: String
+      }
+      let user = User(
+        id: UUID(uuidString: "DEADBEEF-DEAD-BEEF-DEAD-BEEFDEADBEEF")!,
+        name: "Blob",
+        bio: "Blobbed around the world."
       )
+      var otherUser = user
+      expectNoDifference(user, otherUser)
+      otherUser.name += " Jr."
+      withKnownIssue {
+        expectNoDifference(user, otherUser)
+      } matching: {
+        $0.description.hasSuffix(
+          """
+          Difference: …
+
+              ExpectNoDifferenceTests.User(
+                id: UUID(DEADBEEF-DEAD-BEEF-DEAD-BEEFDEADBEEF),
+            −   name: "Blob",
+            +   name: "Blob Jr.",
+                bio: "Blobbed around the world."
+              )
+
+          (First: −, Second: +)
+          """
+        )
+      }
     }
   }
-}
 #endif
